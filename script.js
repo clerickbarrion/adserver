0bc3a766a6e5efb1b839f50f88215f913ef6b2c2
clerickbarrion <clerickbarrion@gmail.com>
2023-11-19 20:26:36 -0500

    astroid destroyer v2: powerups

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// github git api
+// github gist api
 import { Octokit } from "https://esm.sh/@octokit/core"
 const one = 'gh'
 const two = 'p_vXCkFfm51w0aCL'
@@ -16,6 +16,7 @@ const CANVAS_HEIGHT = canvas.height = 720;
 const title = document.getElementById('title')
 const places = document.getElementById('places')
 const playAgainBtn = document.getElementById('play-again')
+const pause = document.getElementById('pause')
 const startScreen = document.getElementById('start-screen')
 const usernameInput = document.getElementById('username')
 const scoreDisplay = document.getElementById('score')
@@ -38,9 +39,9 @@ async function postScores(data){
         gist_id: '779fab2e80cc29dd187a82e1ae8fabd8',
         description: 'updated scores',
         files: {
-          adl: {
-            content: data,
-          },
+            adl: {
+                content: data,
+            },
         },
         headers: {
           'X-GitHub-Api-Version': '2022-11-28'
@@ -57,17 +58,25 @@ usernameInput.addEventListener('keydown', e => {
 })
 playAgainBtn.addEventListener('click', ()=>{
     health = 100;
-    healthDisplay.innerText = Number('100')
+    healthDisplay.innerText = 100
     title.innerText = 'Astroid Destroyer'
     score = 0
-    scoreDisplay.innerText = Number('0')
+    scoreDisplay.innerText = 0
     playAgainBtn.style.display = 'none'
     animate()
+})
+pause.addEventListener('click', () => {
+    pause.blur()
+    game.pause = !game.pause
+    game.pause ? pause.innerText = 'Resume' : pause.innerText = 'Pause'
 })
 // functions
 function spawnLoop(){
     let loop = setInterval(() => {
-        if (health > 0) {game.spawnAstroids()}
+        if (health > 0 && !game.pause) {
+            game.spawnAstroids()
+            game.spawnPowerups()
+        }
     }, 5000)
     return loop
 }
@@ -81,9 +90,11 @@ function animate(loop){
         playAgainBtn.style.display = 'block'
         return
     }
-    ctx.clearRect(0,0,canvas.width,canvas.height)
-    game.update();
-    game.draw(ctx)
+    if (!game.pause) {
+        ctx.clearRect(0,0,canvas.width,canvas.height)
+        game.update();
+        game.draw(ctx)
+    }
     requestAnimationFrame(animate)
 }
 
@@ -98,7 +109,6 @@ function leaderboardInit(scores){
         place.textContent = `${score.name}: ${score.score}`
         places.appendChild(place)
     })
-    return scores
 }
 
 // classes
@@ -108,39 +118,75 @@ class Game {
         this.height = height;
         this.player = new Player(this)
         this.astroids = []
+        this.powerups = []
+        this.scoreIncrement = 10
         this.input = new InputHandler(this)
         this.keys = []
+        this.shield = false
+        this.pause = false
     }
     update(){
         this.player.update();
+
         this.astroids.forEach(astroid => {
             astroid.update()
-            if (this.checkCollision(this.player,astroid)){
-                astroid.delete = true;
-            }
+            if (this.checkCollision(this.player,astroid)){astroid.delete = true;}   
             this.player.projectiles.forEach(projectile => {
                 if (this.checkCollision(projectile,astroid)){
                     astroid.delete = true;
                     projectile.delete = true;
-                    score += 10
+                    score += this.scoreIncrement
                     scoreDisplay.innerText = score
                 }
             })
         })
         this.astroids = this.astroids.filter(astroid => !astroid.delete)
+
+        this.powerups.forEach(powerup => {
+            powerup.update()
+            if (this.checkCollision(this.player,powerup)){
+                switch (powerup.powerup) {
+                    case 'hp':
+                        health += 20;
+                        healthDisplay.innerText = health;
+                        break;
+                    case 'speedboost':
+                        this.player.maxSpeed *= 2;
+                        setTimeout(() => {this.player.maxSpeed /= 2},20000);
+                        break;
+                    case 'x2score':
+                        this.scoreIncrement *= 2;
+                        setTimeout(() => {this.scoreIncrement /= 2}, 30000)
+                        break;
+                    case 'shield':
+                        this.shield = true;
+                        setTimeout(() => {this.shield = false},20000)
+                        break;
+                    case 'x2shot':
+                        this.player.x2shot = true;
+                        setTimeout(() => {this.player.x2shot = false},30000)
+                        break;
+                }
+                powerup.delete = true
+            }
+        })
+        this.powerups = this.powerups.filter(powerup => !powerup.delete)
     }
     draw(context){
         this.player.draw(context);
-        this.astroids.forEach(astroid => {
-            astroid.draw(context)
-        })
+        this.astroids.forEach(astroid => {astroid.draw(context)})
+        this.powerups.forEach(powerup => {powerup.draw(context)})
     }
     spawnAstroids(){
-    for (let i =0; i<5; i++){
+        for (let i =0; i< Math.random() * 6 + 5; i++){
             setTimeout(() => 
                 {this.astroids.push(new Astroid(this))
             }, i * 500)
         }
+    }
+    spawnPowerups(){
+        let rng = Math.floor(Math.random() * 100)
+        if (rng < 26) {this.powerups.push(new Powerups(this))}
     }
     checkCollision(rect1, rect2){
         return (rect1.x < rect2.x + rect2.width &&
@@ -175,6 +221,7 @@ class Player {
         this.maxSpeed = 5
         this.projectiles = []
         this.image = document.getElementById('player')
+        this.x2shot = false
     }
     update() {
         if (this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed
@@ -182,9 +229,7 @@ class Player {
         else this.speedY = 0
         this.y += this.speedY;
 
-        this.projectiles.forEach(projectile => {
-            projectile.update()
-        })
+        this.projectiles.forEach(projectile => {projectile.update()})
         this.projectiles = this.projectiles.filter(projectile => !projectile.delete)
     }
     draw(context){
@@ -196,7 +241,11 @@ class Player {
         })
     }
     shoot(){
-        this.projectiles.push(new Projectile(this.game, this.x,this.y))
+        this.projectiles.push(new Projectile(this.game, this))
+        if (this.x2shot) {
+            this.projectiles.push(new Projectile2(this.game, this, 'up'))
+            this.projectiles.push(new Projectile2(this.game, this, 'down'))
+        }
     }
 }
 
@@ -222,10 +271,10 @@ class InputHandler {
 }
 
 class Projectile {
-    constructor(game,x,y){
+    constructor(game,player){
         this.game = game;
-        this.x = x;
-        this.y = y;
+        this.x = player.x + player.width;
+        this.y = player.y + player.height/2;
         this.width = 10;
         this.height = 3;
         this.speed = 10;
@@ -237,7 +286,19 @@ class Projectile {
     }
     draw(context){
         context.fillStyle = 'red'
-        context.fillRect(this.x + 120, this.y + this.height + 30, this.width, this.height)
+        context.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
+
+class Projectile2 extends Projectile {
+    constructor(game,player,direction){
+        super(game,player)
+        this.direction = direction
+    }
+    update() {
+        this.x += this.speed;
+        this.direction == 'up' ? this.y -= this.speed/2 : this.y += this.speed/2
+        if (this.x > this.game.width * 0.8) this.delete = true;
     }
 }
 
@@ -245,26 +306,51 @@ class Astroid {
     constructor(game){
         this.game = game
         this.image = document.getElementById('astroid')
-        this.x = game.width
-        this.y = Math.random() * game.height
         this.width = 97
         this.height = 71
+        this.x = this.game.width
+        this.y = Math.random() * (this.game.height - this.height)
         this.speed = 5
+        this.healthDecrement = 10
         this.delete = false
     }
     update(){
         this.x -= this.speed
         if (this.x <= 0) {
             this.delete = true;
-            health -= 5
-            healthDisplay.innerText = health
+            if (!this.game.shield){
+                health -= this.healthDecrement
+                healthDisplay.innerText = health
+            }   
         }
     }
-
     draw(context){
         context.fillStyle = 'rgba(255,255,255,0.0)'
         context.fillRect(this.x,this.y,this.width,this.height)
         context.drawImage(this.image, this.x, this.y)
+    }
+}
+
+class Powerups {
+    constructor(game){
+        this.game = game;
+        this.poweruplist = ['hp','speedboost','x2score','shield','x2shot']
+        this.powerup = this.poweruplist[Math.floor(Math.random() * 5)]
+        this.x = game.width;
+        this.width = 50;
+        this.height = 50;
+        this.y = Math.random() * (this.game.height - this.height);
+        this.speed = 6;
+        this.delete = false;
+    }
+    update(){
+        this.x -= this.speed
+        if (this.x <= 0) {this.delete = true;}
+    }
+    draw(context){
+        context.fillStyle = 'rgba(255,255,255,0.0)'
+        context.fillRect(this.x,this.y,this.width,this.height)
+        context.drawImage(document.getElementById(this.powerup), this.x, this.y)
     }
 }
 
